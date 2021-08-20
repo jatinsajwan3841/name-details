29d96aa62f3d1b1b0d7c3a7d6b64033b7f151b2e
jatinsajwan3841 <jatinsajwan3841@gmail.com>
2021-08-20 09:30:25 +0530

    handle not found

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -6,32 +6,39 @@ const Main = () => {
     const [age, setAge] = React.useState('')
     const [nationality, setNationality] = React.useState('')
     const [open, setOpen] = React.useState(false)
+    const [found, setFound] = React.useState(0)
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         const name = e.target.name.value
+        await setFound(0)
         if (name.split(' ').length > 1) {
             alert('Why did you do that? please enter first name only!')
             return
         } else if (name === '') {
             alert('Why did you do that? please enter the first name!')
+            return
         } else {
             let gen = await fetch(`https://api.genderize.io?name=${name}`)
             gen = await gen.json()
-            await setGender(gen)
+            if (gen.gender !== null) setGender(gen)
+            else setFound((prev) => prev + 1)
             let age = await fetch(`https://api.agify.io?name=${name}`)
             age = await age.json()
-            await setAge(age)
+            if (age.age !== null) setAge(age)
+            else setFound((prev) => prev + 1)
             let nationality = await fetch(
                 `https://api.nationalize.io?name=${name}`,
             )
             nationality = await nationality.json()
             if (nationality.country.length === 0) {
-                await setNationality(['oops! could not guess!', ''])
+                setNationality(['oops! could not guess!', ''])
+                setFound((prev) => prev + 1)
             } else {
                 let regionNames = await new Intl.DisplayNames(['en'], {
                     type: 'region',
                 })
-                await setNationality([
+                setNationality([
                     regionNames.of(nationality.country[0]?.country_id),
                     nationality.country[0]?.country_id,
                 ])
@@ -42,6 +49,7 @@ const Main = () => {
         setNationality('')
         setAge('')
         setGender('')
+        setFound(0)
     }
     return (
         <div className="container">
@@ -72,36 +80,43 @@ const Main = () => {
                 />
             </form>
             <div className="output">
-                {gender !== '' && (
-                    <div>
-                        Hello {gender.name}! <br />I guess you are a{' '}
-                        {gender.gender}
-                        {gender.gender === 'male' ? (
-                            <span> &#9794;</span>
-                        ) : (
-                            <span> &#9792;</span>
+                {found === 3 ? (
+                    <> Sorry could not guess, is it even a name? </>
+                ) : (
+                    <>
+                        {' '}
+                        {gender !== '' && (
+                            <div>
+                                Hello {gender.name}! <br />I guess you are a{' '}
+                                {gender.gender}
+                                {gender.gender === 'male' ? (
+                                    <span> &#9794;</span>
+                                ) : (
+                                    <span> &#9792;</span>
+                                )}
+                                .
+                            </div>
                         )}
-                        .
-                    </div>
-                )}
-                {age !== '' && <div>umm maybe {age.age} years old</div>}
-                {nationality !== '' && (
-                    <div>
-                        and probably from {nationality[0]}{' '}
-                        {nationality[1] !== '' && (
-                            <img
-                                src={`https://www.countryflags.io/${nationality[1]}/flat/32.png`}
-                                alt={nationality[0]}
-                                height="32px"
-                                width="32px"
-                                className="flag"
-                            />
+                        {age !== '' && <div>umm maybe {age.age} years old</div>}
+                        {nationality !== '' && (
+                            <div>
+                                and probably from {nationality[0]}{' '}
+                                {nationality[1] !== '' && (
+                                    <img
+                                        src={`https://www.countryflags.io/${nationality[1]}/flat/32.png`}
+                                        alt={nationality[0]}
+                                        height="32px"
+                                        width="32px"
+                                        className="flag"
+                                    />
+                                )}
+                            </div>
                         )}
-                    </div>
+                        <div className="source" onClick={() => setOpen(true)}>
+                            Api sources ?
+                        </div>
+                    </>
                 )}
-                <div className="source" onClick={() => setOpen(true)}>
-                    Api sources ?
-                </div>
             </div>
             {open && (
                 <div className="details" onClick={() => setOpen(false)}>
